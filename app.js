40445af85d808e2c0089357ae423a39449603794
ClaasM <claasmeiners@googlemail.com>
2015-08-29 22:42:49 +0200

    Sollte jetzt gehen..musste manuell zu package.json hinzugefügt werden

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-/**
+
 app.use(seo({
     cacheClient: 'disk',
     cacheDuration: 60 * 60 * 24 * 1000
 }));
-**/
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/sendmail', sendmail);
 
